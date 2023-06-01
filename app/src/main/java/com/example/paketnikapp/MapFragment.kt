8fbc90030664f4ebd897b3666e712afacb6a10c1
sinisaV <sinisa.vucetic@student.um.si>
2023-06-01 15:14:16 +0200

    refactor: deleted unused imports

--- app/src/main/java/com/example/paketnikapp/MapFragment.kt
+++ app/src/main/java/com/example/paketnikapp/MapFragment.kt
@@ -5,11 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.paketnikapp.databinding.FragmentMapBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MapFragment : Fragment(R.layout.fragment_map) {
     private var _binding: FragmentMapBinding? = null
