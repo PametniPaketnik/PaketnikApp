5e169007ef7f6a39150dccc252781f1d143f753c
sinisaV <sinisa.vucetic@student.um.si>
2023-06-04 15:07:40 +0200

    fix: fixed User class

--- lib/src/main/java/com/example/lib/User.kt
+++ lib/src/main/java/com/example/lib/User.kt
@@ -1,13 +1,13 @@
 package com.example.lib
 
 data class User(
-    val username: String, val password: String, val email: String,
-    val isAdmin: Boolean, val firstName: String, val lastName: String,
-    val tel: String, val street: String, val postcode: String,
-    val lat: Double, val lng: Double
+    val _id: String, val username: String, val password: String,
+    val email: String, val isAdmin: Boolean, val firstName: String,
+    val lastName: String, val tel: String, val street: String,
+    val postcode: String, val lat: Double, val lng: Double
 ) {
     constructor() : this(
-        "", "", "",
+        "", "", "", "",
         false, "", "",
         "", "", "", 0.0, 0.0
     )
