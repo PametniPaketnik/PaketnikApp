477d65078087efad58fd97ad6a1e3d87d79de089
sabina <sabina.pauric@student.um.si>
2024-01-14 22:36:56 +0100

    deleted comments and imports

--- app/src/main/java/com/example/paketnikapp/TSPAlgorithmFragment.kt
+++ app/src/main/java/com/example/paketnikapp/TSPAlgorithmFragment.kt
@@ -1,12 +1,10 @@
 package com.example.paketnikapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lib.Location
@@ -74,18 +72,15 @@ class TSPAlgorithmFragment : Fragment(R.layout.fragment_t_s_p_algorithm) {
                     continue
                 }
 
-                // Preberi index in celoten naslov
                 val index = line?.substringBefore(" ")?.trim()
                 val fullAddress = line?.substringAfter(" ")?.trim()
 
-                // Preberi naslednjo vrstico za x in y koordinate
                 val xLine = bufferedReader.readLine()
                 val parts = xLine?.split(" ") ?: emptyList()
                 val xCoordinate = parts.getOrNull(0)?.toDoubleOrNull()
                 val yCoordinate = parts.getOrNull(1)?.toDoubleOrNull()
 
                 if (index != null && fullAddress != null && xCoordinate != null && yCoordinate != null) {
-                    // Ustvari Location objekt in dodaj v seznam
                     locationsList.add(Location(index, fullAddress, xCoordinate, yCoordinate))
                 }
 
